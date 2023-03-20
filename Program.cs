94c8eda755a8d7622377cff449111b7901eeb8e7
MansiRavindraMohan <manasimohan29@gmail.com>
2023-03-20 22:56:18 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab invoice generator");
+            //InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+
+            //double fare = invoice.CalculateFare(2.0, 5);
+            //Console.WriteLine(fare);
+        }
+    }
+}
